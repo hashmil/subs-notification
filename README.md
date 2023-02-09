24fd68affd05cf60eebcee0e7a1809d3bfea7318
Hash Milhan <hashirm@gmail.com>
2023-02-09 23:13:38 +0000

    updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# subs-notification
-Notify 7 days within a subscriptions expire. A web interfect.
+# Subscription Notification Web Interface
+
+Notify 7 days within a subscriptions expire. And a web interfece.
+
+This code is a Flask application that allows you to manage your subscriptions by adding, editing and deleting them. The application uses a SQLite database to store the subscriptions. It also features a homepage that displays all your subscriptions, along with a list of subscriptions that are expiring within the next 7 days. The application has a basic UI that allows you to add and edit your subscriptions, and you can access this UI by visiting the "/add" or "/edit" routes. The "/delete" route allows you to delete subscriptions from the database. The application is configured using a "config.ini" file, and it sends email notifications using the smtplib library. The code uses the schedule library to schedule tasks in the background.
--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import datetime
 from flask import Flask, request, render_template, redirect, url_for
 import queue
 import threading
+import ssl
+from datetime import datetime, timedelta
+from configparser import ConfigParser
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import schedule
+import time
 
 
 app = Flask(__name__)
@@ -28,14 +35,14 @@ def index():
         subscriptions = cursor.fetchall()
 
         # Get the current date and the date 7 days from now
-        current_date = datetime.datetime.now().date()
-        next_week = current_date + datetime.timedelta(days=7)
+        current_date = datetime.now().date()
+        next_week = current_date + timedelta(days=7)
 
         # Filter the subscriptions that are expiring within the next 7 days
-        expiring_subscriptions = [sub for sub in subscriptions if (datetime.datetime.strptime(
-            sub[2], "%Y-%m-%d").date() >= current_date) and (datetime.datetime.strptime(sub[2], "%Y-%m-%d").date() < next_week)]
-        remaining_subscriptions = [sub for sub in subscriptions if datetime.datetime.strptime(
-            sub[2], "%Y-%m-%d").date() not in [datetime.datetime.strptime(x[2], "%Y-%m-%d").date() for x in expiring_subscriptions]]
+        expiring_subscriptions = [sub for sub in subscriptions if (datetime.strptime(
+            sub[2], "%Y-%m-%d").date() >= current_date) and (datetime.strptime(sub[2], "%Y-%m-%d").date() < next_week)]
+        remaining_subscriptions = [sub for sub in subscriptions if datetime.strptime(
+            sub[2], "%Y-%m-%d").date() not in [datetime.strptime(x[2], "%Y-%m-%d").date() for x in expiring_subscriptions]]
 
     return render_template("index.html", expiring_subscriptions=expiring_subscriptions, remaining_subscriptions=remaining_subscriptions)
 
@@ -75,7 +82,7 @@ def edit(id):
                 expiry_date = request.form["expiry_date"]
 
                 # Ensure the expiry date is in the correct format
-                expiry_date = datetime.datetime.strptime(
+                expiry_date = datetime.strptime(
                     expiry_date, "%Y-%m-%d").date().strftime("%Y-%m-%d")
 
                 # Update the subscription in the database
@@ -109,7 +116,63 @@ def delete(id):
 
     return redirect(url_for("index"))
 
+# Email notification
+
+
+def send_email(subject, body, recipient_email, sender_email, password):
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = recipient_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, recipient_email, message.as_string())
+
+
+def check_and_send_email():
+    # Load the email configuration from the config.ini file
+    config = ConfigParser()
+    config.read("config.ini")
+    sender_email = config.get("email", "sender_email")
+    password = config.get("email", "sender_password")
+    recipient_email = config.get("email", "recipient_email")
+
+    with sqlite3.connect("subscriptions.db", detect_types=sqlite3.PARSE_DECLTYPES) as conn:
+        cursor = conn.cursor()
+
+        # Get the subscriptions expiring in the next 7 days
+        expiring_subscriptions = cursor.execute(
+            "SELECT * FROM subscriptions WHERE expiry_date BETWEEN date('now') AND date('now', '+7 days')").fetchall()
+
+        # Only send an email if there are any subscriptions expiring in the next 7 days
+        if expiring_subscriptions:
+            body = "The following subscriptions will be expiring within the next 7 days:\n\n"
+            for subscription in expiring_subscriptions:
+                body += f"Product: {subscription[1]}\nExpiry Date: {subscription[2]}\n\n"
+
+            subject = "Upcoming Subscription Expirations"
+            send_email(subject, body, recipient_email, sender_email, password)
+            print("An email was sent with the list of upcoming expiring subscriptions.")
+        else:
+            print("No expiring subscriptions found within the next 7 days.")
+
+
+# def run_every_day():
+#     check_and_send_email()
+
 
 if __name__ == "__main__":
 
     app.run(debug=True)
+    check_and_send_email()
+
+    # # Schedule the run_every_day function to run every day at 12:00 AM
+    # schedule.every().day.at("23:11").do(run_every_day)
+
+    # # Keep the program running and check for pending tasks every second
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
